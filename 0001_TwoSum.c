5d6c16da13f98073d24cb0876d45e5527af47e1e
mharshr2135 <159751147+harshr2135@users.noreply.github.com>
2024-10-27 20:48:53 +0530

    Update 0001_TwoSum.c

--- 0001_TwoSum.c
+++ 0001_TwoSum.c
@@ -1,6 +1,10 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
+
+//Time Complexity:  O(n^2)
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int* res = (int*) malloc(2*sizeof(int));
     *returnSize = 2;
